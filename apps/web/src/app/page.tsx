89bf573960299ad70435a208fc8c3708dffd245c
J.B.C <juanbicortelezzi@gmail.com>
2023-06-09 22:06:18 +1200

    feat add twemes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,8 +6,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>hello tere</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">hello tere</h1>
     </div>
   );
 }
