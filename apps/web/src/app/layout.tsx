152cf04c6c67d7b81f4cd0da76b4116404d43e56
J.B.C <juanbicortelezzi@gmail.com>
2023-06-11 20:07:30 +1200

    feat add simple example

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -6,6 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // Notice the LSP support when updating className!
     <html lang="en" className="bg-base text-primary">
       <body>{children}</body>
     </html>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,7 +7,10 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">hello tere</h1>
+      <h1 className="text-3xl font-bold">Hello there!</h1>
+      <p>
+        This page&apos;s theme is determined by your colorscheme preference.
+      </p>
     </div>
   );
 }
